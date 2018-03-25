e494b68fdd7a89dae964dc2efd8cea6eb2c1deab
Anjo Tadena <anjo@coderepubliq.com>
2018-03-25 10:46:07 +0800

    Set breadcrumb metabox for child pages.

--- wp-content/themes/fictional-university/page.php
+++ wp-content/themes/fictional-university/page.php
@@ -12,10 +12,20 @@
 		</div>
 
 		<div class="container container--narrow page-section">
+            <?php
+                $parent_page_id = wp_get_post_parent_id(get_the_ID());
 
+                if ($parent_page_id) { // return 0 if dont have a parent
+            ?>
 			<div class="metabox metabox--position-up metabox--with-home-link">
-				<p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+				<p>
+                    <a class="metabox__blog-home-link" href="<?php echo get_permalink($parent_page_id); ?>">
+                        <i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parent_page_id); ?>
+                    </a>
+                    <span class="metabox__main"><?php the_title(); ?></span>
+                </p>
 			</div>
+            <?php } ?>
 
 <!--			<div class="page-links">-->
 <!--				<h2 class="page-links__title"><a href="#">About Us</a></h2>-->
