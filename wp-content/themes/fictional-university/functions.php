11e8b992bad0014b215c39144e915a1bc4adacfe
Anjo Tadena <anjo@coderepubliq.com>
2018-03-25 10:03:36 +0800

    Disable cache for selecteed asset.

--- wp-content/themes/fictional-university/functions.php
+++ wp-content/themes/fictional-university/functions.php
@@ -4,8 +4,8 @@ function fictional_university_assets ()
 {
 	wp_enqueue_style('google_font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 	wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-	wp_enqueue_style('fictional_university_main_style', get_stylesheet_uri());
-	wp_enqueue_script('custom_javascript', get_theme_file_uri('/js/scripts-bundled.js'), null, '1.0.0', true);
+	wp_enqueue_style('fictional_university_main_style', get_stylesheet_uri(), null, microtime());
+	wp_enqueue_script('custom_javascript', get_theme_file_uri('/js/scripts-bundled.js'), null, microtime(), true);
 }
 
 add_action('wp_enqueue_scripts', 'fictional_university_assets');
